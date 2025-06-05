afddbf11b5a92b3f781e963467b0db3e18bb726d
clebercsn <clebernichelli@gmail.com>
2025-06-05 13:41:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 3000;
 
 // Middleware para entender JSON no corpo da requisição
 app.use(express.json());
 
 // Servir arquivos estáticos da pasta public
 app.use(express.static('public'));
-
-// Rota para servir o index.html
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
-});
 
 // Rota POST para receber os dados de geolocalização
 app.post('/api/dados', (req, res) => {
@@ -23,7 +17,8 @@ app.post('/api/dados', (req, res) => {
     res.sendStatus(200);
 });
 
-// Iniciar o servidor
+// Iniciar o servidor na porta definida pela Render ou 3000 localmente
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`);
 });
